dfaf89f09762d2246e793008892626c87e55ddaa
Travis Simpkins <t73designs@yahoo.com>
2019-10-25 16:00:30 -0400

    Static Site Updates

--- src/ArticleView.js
+++ src/ArticleView.js
@@ -1,33 +1,27 @@
+
 import React from 'react'
+import { Link } from 'react-router-dom';
 import CommentsList from './CommentsList.js'
 import './ArticleView.css'
-
 
 export default class ArticleView extends React.Component {
 
 
     render() {
         return (
-            <section id="article-full-container"><section id="article-Full">
+            <div id="article-full-container">
+            <section id="article-Full">
         <div id="articleImage"></div>
         <div id="articleText">
           <h3 id="articleText">Article Headline</h3>
-          <p id="articleText">Article Text
-          
-          
-          ....
-          
-          ....
-          </p>
-          <div id="articleButtons">
-            <button>Favorite Button</button>
-<button>Comment Button</button>
+          <p id="articleText">Article Text</p>
         </div>
+        <div id="articleButtons">
+            <button>Favorite Button</button>
+            <Link to="/dashboard/article/comment"><button>Comment Button</button></Link>
         </div>
         <CommentsList />
             </section>
-            </section>
+            </div>
                 );
             }
-            
-}
