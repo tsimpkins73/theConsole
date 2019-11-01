739ed913ee5953b2b9a7561b84ec244bd5eb4a7d
tsimpkins73 <46323925+tsimpkins73@users.noreply.github.com>
2019-11-01 07:15:55 -0400

    Static Ste Updates

--- src/CategoriesLink.js
+++ src/CategoriesLink.js
@@ -3,9 +3,13 @@ import { Link } from 'react-router-dom';
 import './Sidebar.css'
 
 export default class CategoriesLink extends React.Component {
-    render() {
+    render() {if(typeof this.props.category == "string"){
         return (
             <Link to={"/dashboard/article/"+this.props.category}><h3>{this.props.category}</h3></Link>
         );
+    }else{
+        
+    }
+        
     }
 }
