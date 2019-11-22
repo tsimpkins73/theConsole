3d167246407f7b6f0933d1d5d56187132ea65fb3
Travis Simpkins <t73designs@yahoo.com>
2019-11-22 15:47:52 -0500

    Styling Updates

--- src/SearchDiv.js
+++ src/SearchDiv.js
@@ -1,24 +1,15 @@
 import React from 'react'
-<<<<<<< HEAD
-import './Sidebar.css'
-import SearchBar from './SearchBar.js'
-=======
 import './css/Sidebar.css'
->>>>>>> 5c21c2d6d090fd3a8568570d0ee7a7f30f232b3f
 
 export default class SearchDiv extends React.Component {
     render() {
 
         return (
             <div id="searchDiv">
-<<<<<<< HEAD
-            <SearchBar />
-=======
             <form onSubmit={this.props.handleSearchForm}>
                 <input type="text" className="input" name="searchTerm" placeholder="Search..." />
                 <button type="submit">Search</button>
                 </form>
->>>>>>> 5c21c2d6d090fd3a8568570d0ee7a7f30f232b3f
           </div>
         );
     }
