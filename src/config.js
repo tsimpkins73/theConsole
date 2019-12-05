1849fe6c42e8e266a9d39258d31403ad35f2ef87
Travis Simpkins <t73designs@yahoo.com>
2019-12-05 14:08:33 -0500

    API Hookup

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api',
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'https://the-console.herokuapp.com/api',
     TOKEN_KEY: 'theConsole-auth-token',
   }
