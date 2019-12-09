83e7d9154bf01ce2905e0dbc362f8fcbe85d6d65
Travis Simpkins <t73designs@yahoo.com>
2019-12-09 11:57:41 -0500

    API Hookup

--- src/ArticleList.js
+++ src/ArticleList.js
@@ -1,36 +1,46 @@
 import React from 'react'
 import ArticlePreview from "./ArticlePreview.js";
 import './css/ArticleList.css'
-import { API_BASE_URL } from './config'
+import {
+    API_BASE_URL
+} from './config'
 
 export default class ArticleList extends React.Component {
-constructor(props){
-    super(props);
-    this.state= {
-        articles:[],
-        categoryId: null,
-    };
-}
+    constructor(props) {
+        super(props);
+        this.state = {
+            articles: [],
+            categoryId: null,
+        };
+    }
 
-fetchData(){
-    fetch(`${API_BASE_URL}/articles/category/${this.props.categoryId}`)
-        .then(response => response.json())
-        .then((articles) => { this.setState({ articles, categoryId:this.props.categoryId }); });
-}
-    componentDidMount () {
-        if (this.props.categoryId){
-            this.fetchData();         
-        }
-        else{
-            this.setState({articles:this.props.articles})
+    fetchData() {
+        fetch(`${API_BASE_URL}/articles/category/${this.props.categoryId}`)
+            .then(response => response.json())
+            .then((articles) => {
+                this.setState({
+                    articles,
+                    categoryId: this.props.categoryId
+                });
+            });
+    }
+    componentDidMount() {
+        if (this.props.categoryId) {
+            this.fetchData();
+        } else {
+            this.setState({
+                articles: this.props.articles
+            })
         }
     }
-componentDidUpdate () {
-  if (props.match.params.categoryId !== this.props.categoryId){
-    this.setState({categoryId:props.match.params.categoryId});    
-    this.fetchData();         
+    componentDidUpdate() {
+        if (props.match.params.categoryId !== this.props.categoryId) {
+            this.setState({
+                categoryId: props.match.params.categoryId
+            });
+            this.fetchData();
+        }
     }
-}
 
     render() {
         let articles = this.state.articles;
@@ -38,11 +48,16 @@ componentDidUpdate () {
             articles = this.props.articles.filter(article => article.text.indexOf(this.props.searchterm) >= 0)
         }
         return (
-            <section id="ArticleList">
-                {articles.map(function (article) {
-                    return <ArticlePreview article={article} />;
-                })}
-            </section>
+            console.log(articles) <
+            section id = "ArticleList" > {
+                articles.map(function (article) {
+                    return <ArticlePreview article = {
+                        article
+                    }
+                    />;
+                })
+            } <
+            /section>
         );
     }
 }
