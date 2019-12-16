b0f10750d430b9ee17d52a702933f604f4b5b3ce
tsimpkins73 <46323925+tsimpkins73@users.noreply.github.com>
2019-12-16 00:13:46 -0500

    API Hookup

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -37,7 +37,7 @@ export default class LoginForm extends React.Component {
         return (
             <section class="LandingContainer">
                 <div class="LoginForm" > <h1 id="HeaderText"> Welcome, Please Sign In! </h1>
-                Guests, please login with Username: GenBlanc and Password: Password34!. 
+                Guests, please login with Username: GenBlanc and Password: Password34! 
                     <form onSubmit={this.handleSubmitJwtAuth}>
                         <div>
                             <label for="username" > Username </label>
