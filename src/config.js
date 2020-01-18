11ee20a2a7ef966718b76c5fc3997d6fb82e0cab
tsimpkins73 <46323925+tsimpkins73@users.noreply.github.com>
2020-01-18 00:04:13 -0500

    Env

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    PORT: process.env.PORT
-    NODE_ENV: process.env.NODE_ENV
-    API_BASE_URL: process.env.REACT_APP_API_BASE_URL
-    DB_URL: process.env.DB_URL
+    PORT: process.env.PORT,
+    NODE_ENV: process.env.NODE_ENV,
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL,
+    DB_URL: process.env.DB_URL,
     TOKEN_KEY: 'theConsole-auth-token',
   }
